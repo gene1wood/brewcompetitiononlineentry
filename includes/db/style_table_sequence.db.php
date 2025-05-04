22790df16fd3205c44dac1af987b97f893919e7d
Gene Wood <gene_wood@cementhorizon.com>
2025-05-03 18:11:44 -0700

    Update style to table map for 2024

--- includes/db/style_table_sequence.db.php
+++ includes/db/style_table_sequence.db.php
@@ -3,36 +3,37 @@ include (DB.'output_pullsheets.db.php');
 include (INCLUDES.'db_tables.inc.php');
 
 $style_sequence_map = array(
-	1 =>  array("01A", "01B", "01C", "01D"),
-	2 =>  array("02A", "03A", "03B"),
-	3 =>  array("11A", "11B", "11C", "12A", "12B"),
-	4 =>  array("04A", "04B", "04C"),
-	5 =>  array("02B", "03C", "06B", "06C", "07A", "07B"),
-	6 =>  array("02C", "03D", "08A", "08B"),
-	7 =>  array("09A", "09B", "09C"),
-	8 =>  array("05A", "05B", "05C", "05D"),
-	9 =>  array("10A", "10C"),
-	10 => array("14B", "14C", "15A"),
-	11 => array("18A", "18B"),
-	12 => array("19A", "19B", "19C"),
-	13 => array("13A", "13B", "13C"),
-	14 => array("15B", "15C", "16A", "16B"),
-	15 => array("20A", "20B", "20C"),
-	16 => array("21A"),
-	17 => array("21B1", "21B", "21B2"),
-	18 => array("17B", "17C", "17D", "22B", "22D"),
-	19 => array("25A", "25C", "26B", "26C", "26D"),
-	20 => array("25B"),
-	21 => array("24A", "24B", "24C", "26A"),
-	22 => array("28A", "28B", "28C", "28D"),
-	23 => array("29A", "29B", "29C", "29D"),
-	24 => array("30A", "30C", "30D", "31A", "32A"),
-	25 => array("33A", "33B"),
-	26 => array("27A3", "27A4", "27A5", "27A6", "27A7", "27A8"),
-	27 => array("34A", "34B", "34C"),
-	28 => array("23A", "23B", "23D", "23E", "23F", "23G"),
-	29 => array("21C"),
-	30 => array("22A")
+	1 => array("01A", "01B", "01C"),
+	2 => array("02A"),
+	3 => array("03A", "03B", "04A", "04C"),
+	4 => array("05B", "05C", "05D"),
+	5 => array("11A", "11B", "11C"),
+	6 => array("12A", "12B","12C"),
+	7 => array("02B", "03C", "06A", "06C"),
+	8 => array("07A", "07B"),
+	9 => array("02C", "03D", "08A", "08B"),
+	10 => array("01D", "10A", "10B"),
+	11 => array("13A", "13B", "13C"),
+	12 => array("14A", "14C", "15A"),
+	13 => array("15B", "15C", "16A", "16B", "16C", "16D"),
+	14 => array("18A", "18B"),
+	15 => array("19A","19B", "19C"),
+	16 => array("20A", "20B"),
+	17 => array("20C"),
+	18 => array("21A"),
+	19 => array("21B1", "21B", "21B2", "21B4", "21B5", "21C"),
+	20 => array("22A", "22B"),
+	21 => array("09A", "09C", "26D"),
+	22 => array("17A", "17B", "17C", "17D"),
+	23 => array("24A", "24B", "24C", "25A", "26A"),
+	24 => array("25B", "25C", "26B", "26C"),
+	25 => array("23F", "29A", "29B", "29C", "31A", "31B", "32A"),
+	26 => array("30A", "30C", "30D"),
+	27 => array("33A", "33B"),
+	28 => array("28A", "28B", "28C"),
+	29 => array("23A", "23B", "23C", "23D", "23E", "23G"),
+	30 => array("27A1", "27A2", "27A", "27A4", "27A5", "27A6", "27A7", "27A8", "27A9"),
+	31 => array("34A", "34B", "34C")
 );
 
 $style_table_map = array();
@@ -76,10 +77,14 @@ foreach ($style_brew_map as $brewing_id => $styles_id) {
 	$style_entry_map[$styles_id][] = $brewing_id;
 }
 $entry_sequence_map = array();
+// error_log(print_r($style_id_catsubcat_map, true));
 foreach ($style_sequence_map as $table => $style_sequence) {
 	$sequence = 0;
 	foreach ($style_sequence as $style_catsubcat) {
 		// Skip styles for which we have not entries
+		if (!array_key_exists($style_catsubcat, $style_id_catsubcat_map)) {
+			error_log("Missing style_catsubcat $style_catsubcat from table style_id_catsubcat_map");
+		}
 		if (array_key_exists($style_id_catsubcat_map[$style_catsubcat], $style_entry_map)) {
 			foreach ($style_entry_map[$style_id_catsubcat_map[$style_catsubcat]] as $brewing_id) {
 				$sequence++;
